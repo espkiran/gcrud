60dcad222036598737afc38d34acde890eee543d
espkiran <32625636+espkiran@users.noreply.github.com>
2025-02-15 06:13:20 +0545

    Update app1.js

--- app1.js
+++ app1.js
@@ -40,7 +40,7 @@ document.getElementById('userForm').addEventListener('submit', async (e) => {
 
       if (!counterDoc.exists()) {
         // Initialize the counter if it doesn't exist
-        nextSerialNumber = 001;
+        nextSerialNumber = 1;
         transaction.set(counterDocRef, { value: nextSerialNumber });
       } else {
         // Increment the counter
